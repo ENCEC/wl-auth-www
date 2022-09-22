567735a2e894e97c176aed214545135ccef970a0
zuojuntao <2275958074@qq.com>
2022-09-22 09:26:53 +0800

    去掉表格固定高度，展示十行

--- src/views/menu-manage/config-data.js
+++ src/views/menu-manage/config-data.js
@@ -94,8 +94,8 @@ export const tableConfig = {
   highlightCurrentRow: true, // 是否支持当前行高亮显示
   mutiSelect: false, // 是否支持列表项选中功能
   indexShow: true,
-  pagination: true,
-  height: '400px'
+  pagination: true
+  // height: '400px'
 };
 
 // 表格列
